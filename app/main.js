6ef14940bd116a4daa4a953884357276b11843e5
nuochong <nuo.chong@163.com>
2018-11-02 23:24:11 +0800

    iconfont update

--- app/main.js
+++ app/main.js
@@ -23,6 +23,62 @@ appShortcuts.available().then(function(available) {
   }
 });
 
+
+import {setActivityCallbacks, AndroidActivityCallbacks} from "ui/frame";
+const View = android.view.View;
+const Typeface =  android.graphics.Typeface;
+
+
+const LinearLayout = android.widget.LinearLayout;
+const LayoutParams = android.widget.LinearLayout.LayoutParams;
+const TextView = android.widget.TextView;
+const Button = android.widget.Button;
+const Gravity = android.view.Gravity;
+
+const MainActivity = android.app.Activity.extend("com.tns.NativeScriptActivity", {
+  onCreate: function (savedInstanceState) {
+    this.super.onCreate(savedInstance);
+
+        // creating LinearLayout
+        let linLayout = new LinearLayout(this);
+        // specifying vertical orientation
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // creating LayoutParams  
+        let linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT); 
+        // set LinearLayout as a root element of the screen 
+        setContentView(linLayout, linLayoutParam);
+
+        let lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        let tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        let btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
+
+
+        let leftMarginParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        let btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        let rightGravityParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        let btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
+}
+});
+
 //import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
 new Vue({
