6f5b518dad48050230d4b956adfc9abacaa154d4
nuochong <nuo.chong@163.com>
2019-01-26 23:42:38 +0800

    debug

--- app/components/example/refresh.vue
+++ app/components/example/refresh.vue
@@ -7,11 +7,11 @@
             <Label class="first" text="first" height="70" backgroundColor="#43b883" ref="ht" flexShrink="0"/>
             <Label text="second" height="70" backgroundColor="#1c6b48" flexShrink="0"/>
             <!-- <Label text="third" height="100%" backgroundColor="#289062" /> -->
-            <ListView for="item in listArr" ref="list" @pan="onListPane(side,$event)">
+            <!-- <ListView for="item in listArr" ref="list" @pan="onListPane(side,$event)">
                     <v-template>
                         <Label :text="item" class="icon" />
                     </v-template>
-                </ListView>
+                </ListView> -->
         </FlexboxLayout>
         <!-- <GridLayout colums="*" rows="*">
             <Label class="message" :text="msg" col="0" row="0" />
@@ -58,9 +58,20 @@ export default {
     },
     onListPane(side,args){
       let list = this.$refs.list.nativeView;
-      console.log('kkkk',list.effectiveMarginTop);
-      list.scrollToIndex(500)
+      console.log('kkk',this.getScrollY());
+      //console.log('kkkk',list.android.smoothScrollToPosition(500));
+      //list.scrollToIndex(500)
+    },
+    getScrollY() {
+      let mListView = this.$refs.list.nativeView.android;
+    let c = mListView.getChildAt(0);
+    if (c == null) {
+        return 0;
     }
+    let firstVisiblePosition = mListView.getFirstVisiblePosition();
+    let top = c.getTop();
+    return -top + firstVisiblePosition * c.getHeight() ;
+}
   }
 };
 </script>
