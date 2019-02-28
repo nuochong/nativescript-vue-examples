fbc2c6ab5b2dd2a047505815ae3f3b285648700d
nuochong <nuo.chong@163.com>
2019-02-28 23:23:59 +0800

    debug

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,17 +1,17 @@
 <template>
   <Page>
     <ActionBar class="action-bar" title="Welcome to NativeScript-Vue!">
-      <ActionItem @tap="onTapEdit" v-show="!isEditing" ios.systemIcon="2" ios.position="right" android.systemIcon="ic_menu_edit" />
+      <!-- <ActionItem @tap="onTapEdit" v-show="!isEditing" ios.systemIcon="2" ios.position="right" android.systemIcon="ic_menu_edit" />
       <ActionItem @tap="onTapSave" v-show="isEditing" ios.systemIcon="3" ios.position="right" android.systemIcon="ic_menu_save" />
-      <ActionItem @tap="onTapCancel" v-show="isEditing" ios.systemIcon="1" android.systemIcon="ic_menu_close_clear_cancel" />
+      <ActionItem @tap="onTapCancel" v-show="isEditing" ios.systemIcon="1" android.systemIcon="ic_menu_close_clear_cancel" /> -->
     </ActionBar>
     <ScrollView>
       <!--<GridLayout colums="*" rows="*">-->
-      <!-- <StackLayout>
+      <StackLayout>
         <Label class="message" :text="msg" col="0" row="0" />
         <Button class="btn btn-primary" text="示例" @tap="example" />
         <Button class="btn btn-primary dd" text="应用" @tap="project" />
-      </StackLayout> -->
+      </StackLayout>
       <!-- </GridLayout> -->
 
     </ScrollView>
