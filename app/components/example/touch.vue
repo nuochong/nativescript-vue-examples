7c5d28986d52da575ed6215f0fe75b99fd29b119
nuochong <nuo.chong@163.com>
2019-04-26 11:01:52 +0800

    debug

--- app/components/example/touch.vue
+++ app/components/example/touch.vue
@@ -1,8 +1,8 @@
 <template>
     <Page loaded="pageLoaded" ref="page">
-        <ActionBar class="action-bar" title="Hello">
+        <!-- <ActionBar class="action-bar" title="Hello">
             <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$navigateBack" />
-        </ActionBar>
+        </ActionBar> -->
         <GridLayout colums="*" rows="*">
             <!-- <Button class="btn btn-primary m-t-20" ref="btn" text="触摸" @tap="touch" /> -->
             <!-- <Button class="btn btn-primary m-t-20" ref="btn" text="触摸" /> -->
