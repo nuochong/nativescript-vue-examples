79d23be51a0727e0c3e10976db8a099f084c420d
nuochong <nuo.chong@163.com>
2019-09-01 15:23:47 +0800

    update drawer

--- app/components/example/multi-drawer/sidebar.vue
+++ app/components/example/multi-drawer/sidebar.vue
@@ -66,71 +66,54 @@ export default {
 .fa {
   font-family: 'FontAwesome';
 }
-
-.page-content .page-placeholder {
-  color: #d7d7d7;
-  font-size: 20;
-  vertical-align: center;
-  horizontal-align: center;
+.page-content {
+  .page-placeholder {
+    color: #d7d7d7;
+    font-size: 20;
+    vertical-align: center;
+    horizontal-align: center;
+  }
+  .page-icon {
+    font-size: 72;
+    vertical-align: top;
+    horizontal-align: center;
+    color: #d7d7d7;
+    margin-top: 20%;
+  }
 }
-.page-content .page-icon {
-  font-size: 72;
-  vertical-align: top;
-  horizontal-align: center;
-  color: #d7d7d7;
-  margin-top: 20%;
-}
-
 .sidedrawer {
   margin-bottom: -100;
+  &.sidedrawer-left {
+    background-color: #3a53ff;
+    .sidedrawer-header-image {
+      color: #f8f8f8;
+      height: 60;
+      width: 60;
+      font-size: 60;
+      padding: 0;
+      margin-bottom: 5;
+      margin-top: 20;
+    }
+    .footnote {
+      color: rgba(255, 255, 255, 0.5);
+    }
+  }
+  .sidedrawer-header {
+    background-color: #3a53ff;
+    .sidedrawer-header-brand {
+      color: #fff;
+    }
+  }
+  .sidedrawer-content {
+    background-color: #fff;
+  }
 }
 .sidedrawer-list-item-icon,
 .sidedrawer-list-item {
   color: #022734;
 }
-.sidedrawer.sidedrawer-left {
-  background-color: #3a53ff;
-}
-.sidedrawer.sidedrawer-left .sidedrawer-header-image {
-  color: #f8f8f8;
-  height: 60;
-  width: 60;
-  font-size: 60;
-  padding: 0;
-  margin-bottom: 5;
-  margin-top: 20;
-}
-.sidedrawer.sidedrawer-left .footnote {
-  color: rgba(255, 255, 255, 0.5);
-}
-.sidedrawer .sidedrawer-header {
-  background-color: #3a53ff;
-}
-.sidedrawer .sidedrawer-header .sidedrawer-header-brand {
-  color: #fff;
-}
-.sidedrawer .sidedrawer-content {
-  background-color: #fff;
-}
-.sidedrawer .sidedrawer-content .sidedrawer-list-item {
-  padding-left: 15;
-}
-.sidedrawer .sidedrawer-content .sidedrawer-list-item label {
-  vertical-align: center;
-  color: #022734;
-}
-.sidedrawer .sidedrawer-content .sidedrawer-list-item .fa {
-  width: 20;
-  margin-right: 10;
-}
-.sidedrawer .sidedrawer-content .sidedrawer-list-item.selected {
-  background-color: #f8f8f8;
-}
-.sidedrawer .sidedrawer-content .sidedrawer-list-item.selected label {
-  color: #3a53ff;
-}
 @font-face {
-  font-family: 'iconfont'; /* project id 898591 */
+  font-family: 'iconfont';
   src: url('//at.alicdn.com/t/font_898591_b1zihxnkeq6.eot');
   src: url('//at.alicdn.com/t/font_898591_b1zihxnkeq6.eot?#iefix') format('embedded-opentype'),
     url('//at.alicdn.com/t/font_898591_b1zihxnkeq6.woff') format('woff'), url('//at.alicdn.com/t/font_898591_b1zihxnkeq6.ttf') format('truetype'),
