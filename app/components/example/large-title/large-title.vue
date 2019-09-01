a4150500aa90c1bf48aba2e0fdeecd92eef4aa13
nuochong <nuo.chong@163.com>
2019-09-01 15:50:25 +0800

    udpate larg-title

--- app/components/example/large-title/large-title.vue
+++ app/components/example/large-title/large-title.vue
@@ -34,9 +34,38 @@
 export default {
   data() {
     return {
-      title: 'My title',
+      title: '愿与你一起看尽人世间的繁华',
       isFlat: true,
-      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+      content: 
+    `如果 思念有声音，那便是窗前的风声，丝丝入耳。
+
+    阳光灿烂，只因为那一路，有你相伴。
+
+    我曾一度认为，最有意义的远行是背着背包去 自己想去的地方。
+
+    直到遇见你，我才深切的明白，是与你手牵手走过的距离。
+
+    我曾一度认为，最富有深情的陪伴是一起去 爱情海的徜徉。
+
+    直到遇见你，我才深切明白，是与你相伴渡过的每一个夜晚。
+
+    愿与你一起看尽人世间的繁华。
+
+    愿与你风烛残年，愿与你一起分享喜怒哀乐。
+
+    与你一起走过似水流年。
+
+    看尽灯火阑珊的喧嚣世界，陪你一起怀念记忆中的小桥流水人家。
+
+    闭着眼睛聆听着你的风花雪月，脑海中想着你应该拥有的模样。
+
+    我的世界，因为有你的陪伴，分外绚烂。
+
+    你路过我的 人生，惊艳了我的时光。
+
+    不管怎样，时至今日，我如此爱你。
+
+    希望在你也步入爱的圣殿后，还能听到你的声音。`
     };
   },
   methods: {
@@ -90,7 +119,7 @@ export default {
   border-bottom-width: 1;
   border-bottom-color: rgba(0, 0, 0, 0.2);
   label {
-    font-size: 36;
+    font-size: 24;
     font-weight: bold;
     margin-left: 20;
   }
