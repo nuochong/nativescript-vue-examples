3de04fbcbf77288136f9d5e66c549fb0716a376c
nuochong <nuo.chong@163.com>
2019-09-06 03:00:10 +0800

    update large-title

--- app/components/example/large-title/large-title.vue
+++ app/components/example/large-title/large-title.vue
@@ -21,9 +21,6 @@
           <Label textWrap="true" :text="content" class="description-label" />
           <Label textWrap="true" :text="content" class="description-label" />
           <Label textWrap="true" :text="content" class="description-label" />
-          <Label textWrap="true" :text="content" class="description-label" />
-          <Label textWrap="true" :text="content" class="description-label" />
-          <Label textWrap="true" :text="content" class="description-label" />
         </StackLayout>
       </StackLayout>
     </ScrollView>
@@ -36,36 +33,35 @@ export default {
     return {
       title: '愿与你一起看尽人世间的繁华',
       isFlat: true,
-      content: 
-    `如果 思念有声音，那便是窗前的风声，丝丝入耳。
+      content: `如果 思念有声音，那便是窗前的风声，丝丝入耳。
 
-    阳光灿烂，只因为那一路，有你相伴。
+阳光灿烂，只因为那一路，有你相伴。
 
-    我曾一度认为，最有意义的远行是背着背包去 自己想去的地方。
+我曾一度认为，最有意义的远行是背着背包去 自己想去的地方。
 
-    直到遇见你，我才深切的明白，是与你手牵手走过的距离。
+直到遇见你，我才深切的明白，是与你手牵手走过的距离。
 
-    我曾一度认为，最富有深情的陪伴是一起去 爱情海的徜徉。
+我曾一度认为，最富有深情的陪伴是一起去 爱情海的徜徉。
 
-    直到遇见你，我才深切明白，是与你相伴渡过的每一个夜晚。
+直到遇见你，我才深切明白，是与你相伴渡过的每一个夜晚。
 
-    愿与你一起看尽人世间的繁华。
+愿与你一起看尽人世间的繁华。
 
-    愿与你风烛残年，愿与你一起分享喜怒哀乐。
+愿与你风烛残年，愿与你一起分享喜怒哀乐。
 
-    与你一起走过似水流年。
+与你一起走过似水流年。
 
-    看尽灯火阑珊的喧嚣世界，陪你一起怀念记忆中的小桥流水人家。
+看尽灯火阑珊的喧嚣世界，陪你一起怀念记忆中的小桥流水人家。
 
-    闭着眼睛聆听着你的风花雪月，脑海中想着你应该拥有的模样。
+闭着眼睛聆听着你的风花雪月，脑海中想着你应该拥有的模样。
 
-    我的世界，因为有你的陪伴，分外绚烂。
+我的世界，因为有你的陪伴，分外绚烂。
 
-    你路过我的 人生，惊艳了我的时光。
+你路过我的 人生，惊艳了我的时光。
 
-    不管怎样，时至今日，我如此爱你。
+不管怎样，时至今日，我如此爱你。
 
-    希望在你也步入爱的圣殿后，还能听到你的声音。`
+希望在你也步入爱的圣殿后，还能听到你的声音。`
     };
   },
   methods: {
@@ -80,65 +76,66 @@ export default {
       }
     },
     someMethod() {
-      console.log('refresh was tapped');
+      console.log('轻敲刷新');
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-// .page {
-//   background-color: #f7f7f7;
-// }
-.container {
-  margin-top: 0;
-}
-// .content {
-//   background-color: #fff;
-//   padding: 20;
-//   font-size: 20;
-// }
-.action-bar {
-  // background-color: #f7f7f7;
-  // border-width: 0;
-  // margin-bottom: 0;
-  label {
-    font-weight: bold;
-    opacity: 0;
+.page {
+  //background-color: #f7f7f7;
+  .action-bar {
+    // background-color: #f7f7f7;
+    border-width: 0;
+    margin-bottom: 0;
+    label {
+      font-weight: bold;
+      opacity: 0;
+    }
+    .hidden {
+      animation-name: show;
+      animation-duration: 0.2;
+      animation-fill-mode: forwards;
+      animation-direction: reverse;
+    }
+    .visible {
+      animation-name: show;
+      animation-duration: 0.2;
+      animation-fill-mode: forwards;
+    }
   }
-}
-.header {
-  margin-top: 0;
-  margin-bottom: 0;
-  margin-left: -16;
-  margin-right: -16;
-  padding-top: 8;
-  padding-bottom: 8;
-  padding-left: 16;
-  padding-right: 16;
-  border-bottom-width: 1;
-  border-bottom-color: rgba(0, 0, 0, 0.2);
-  label {
-    font-size: 24;
-    font-weight: bold;
-    margin-left: 20;
+  .container {
+    margin-top: 0;
+    .header {
+      margin-top: 0;
+      margin-bottom: 0;
+      margin-left: -16;
+      margin-right: -16;
+      padding-top: 8;
+      padding-bottom: 8;
+      padding-left: 16;
+      padding-right: 16;
+      border-bottom-width: 1;
+      border-bottom-color: rgba(0, 0, 0, 0.2);
+      label {
+        font-size: 24;
+        font-weight: bold;
+        margin-left: 20;
+        color: whitesmoke;
+      }
+    }
+    .content {
+      padding: 20;
+      // background-color: #fff;
+      font-size: 20;
+      .description-label {
+        margin-bottom: 16;
+        line-height: 3;
+        // color: rgba(0, 0, 0, 0.87);
+      }
+    }
   }
-}
-.description-label {
-  margin-bottom: 16;
-  line-height: 3;
-  color: rgba(0, 0, 0, 0.87);
-}
-.hidden {
-  animation-name: show;
-  animation-duration: 0.2;
-  animation-fill-mode: forwards;
-  animation-direction: reverse;
-}
-.visible {
-  animation-name: show;
-  animation-duration: 0.2;
-  animation-fill-mode: forwards;
 }
 
 @keyframes show {
