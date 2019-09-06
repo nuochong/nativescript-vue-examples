e24c87f0e660534783951bdadbcfcbeae7e28aa1
nuochong <nuo.chong@163.com>
2019-09-06 13:50:29 +0800

    update axios

--- app/components/example/request/request-axios.vue
+++ app/components/example/request/request-axios.vue
@@ -3,16 +3,18 @@
     <ActionBarSecond :title="title" />
 
     <StackLayout>
-      <Label text="Hello world !" />
-      <!-- <Button class="btn btn-primary" text="Get from URL" @tap="axiosGet" /> -->
+      <Button class="btn btn-primary" text="Get from URL" @tap="axiosGet" />
       <Button class="btn btn-primary" text="Post JSON" @tap="axiosPost" />
-
-      <Label textWrap="true">
-        <FormattedString>
-          <Span text="Data :" />
-          <Span :text="data" />
-        </FormattedString>
-      </Label>
+      <ScrollView ref="scrollView">
+        <StackLayout class="content">
+          <Label textWrap="true">
+            <FormattedString>
+              <Span text="返回的数据是如下：" />
+              <Span :text="data" />
+            </FormattedString>
+          </Label>
+        </StackLayout>
+      </ScrollView>
     </StackLayout>
   </Page>
 </template>
@@ -31,17 +33,17 @@ export default {
     };
   },
   methods: {
-    // axiosGet() {
-    //   this.axios
-    //     .get('https://httpbin.org/get')
-    //     .then(response => {
-    //       console.dir(response.data);
-    //       this.data = JSON.stringify(response.data);
-    //     })
-    //     .catch(err => {
-    //       console.log(err);
-    //     });
-    // },
+    axiosGet() {
+      this.axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          console.dir('axio获取的get请求是：', response.data);
+          this.data = JSON.stringify(response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     axiosPost() {
       this.axios
         .post('https://www.easy-mock.com/mock/5d60c006f81908124e0fcc23/example/upload', {
@@ -52,7 +54,7 @@ export default {
           }
         })
         .then(response => {
-          console.dir(response.data);
+          console.dir('axio获取的post请求是：', response.data);
           this.data = JSON.stringify(response.data);
         })
         .catch(err => {
@@ -63,3 +65,8 @@ export default {
 };
 </script>
 
+<style scoped lang="scss">
+.content {
+  padding: 15;
+}
+</style>
