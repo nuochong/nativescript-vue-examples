d2d801f961a4884f385c83e4398295dbd9dc3c86
nuochong <nuo.chong@163.com>
2019-09-07 12:53:25 +0800

    update qrcode

--- app/components/example/qrcode/qrcode.vue
+++ app/components/example/qrcode/qrcode.vue
@@ -9,7 +9,7 @@
       <BarcodeScanner
         row="1"
         height="300"
-        formats="QR_CODE, EAN_13, UPC_A"
+        formats="QR_CODE"
         beepOnScan="true"
         reportDuplicates="true"
         preferFrontCamera="false"
@@ -31,7 +31,7 @@ export default {
   },
   data() {
     return {
-      readme:'https://github.com/eddyverbruggen/nativescript-barcodescanner'
+      readme: 'https://github.com/eddyverbruggen/nativescript-barcodescanner'
     };
   },
   mounted() {},
@@ -40,21 +40,21 @@ export default {
       console.log('开始扫码');
       barcodescanner
         .scan({
-          formats: 'QR_CODE,PDF_417', // Pass in of you want to restrict scanning to certain types
-          cancelLabel: 'EXIT. Also, try the volume buttons!', // iOS only, default 'Close'
-          cancelLabelBackgroundColor: '#333333', // iOS only, default '#000000' (black)
-          message: 'Use the volume buttons for extra light', // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
-          showFlipCameraButton: true, // default false
-          preferFrontCamera: false, // default false
-          showTorchButton: true, // default false
-          beepOnScan: true, // Play or Suppress beep on scan (default true)
-          torchOn: false, // launch with the flashlight on (default false)
+          formats: 'QR_CODE', // 如果你想把扫描限制在某些类型：PDF_417
+          cancelLabel: 'EXIT. Also, try the volume buttons!', // 只有iOS，默认“关闭”
+          cancelLabelBackgroundColor: '#333333', // 只有iOS，默认 '#000000' (black)
+          message: 'Use the volume buttons for extra light', // 只有Android, 默认是 '在取景器矩形内放置条形码进行扫描。'
+          showFlipCameraButton: true, // 默认 false
+          preferFrontCamera: false, // 默认 false
+          showTorchButton: true, // 默认 false
+          beepOnScan: true, // 在扫描时播放或抑制“哔”声 (默认 true)
+          torchOn: false, // 开着手电筒发射 (默认 false)
           closeCallback: function() {
             console.log('Scanner closed');
-          }, // invoked when the scanner was closed (success or abort)
-          resultDisplayDuration: 500, // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
-          orientation: 'landscape', // Android only, optionally lock the orientation to either "portrait" or "landscape"
-          openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
+          }, // 当扫描器关闭时调用(成功或中止)
+          resultDisplayDuration: 500, // 只有Android, 默认1500 (ms)，设置为0以禁用回显扫描文本
+          orientation: 'portrait', // 只有Android, 可选锁定方向为“portrait”或“landscape”
+          openSettingsIfPermissionWasPreviouslyDenied: true // 在iOS上，如果之前被拒绝访问，你可以将用户发送到设置应用程序
         })
         .then(
           function(result) {
