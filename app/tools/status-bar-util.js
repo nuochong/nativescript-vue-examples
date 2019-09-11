8248e38748ccff1d9a67c229634156986539db87
nuochong <nuo.chong@163.com>
2019-09-12 00:58:15 +0800

    fix ios bug

--- app/tools/status-bar-util.js
+++ app/tools/status-bar-util.js
@@ -31,38 +31,40 @@ function setStatusBarColors() {
 }
 //设置安卓顶部导航背景颜色并根据亮色和暗色自动切换导航字体颜色
 function setStatusBarFontColors(color = 'default') {
-  const View = android.view.View;
-  const window = application.android.startActivity.getWindow();
-  if (color == 'default') {
-    window.setStatusBarColor(defaultColor);
-  } else {
-    //存储默认背景色
-    defaultColor = window.getStatusBarColor();
-    const argb = new Color(color);
-    window.setStatusBarColor(argb._argb);
-  }
+  if (application.android) {
+    const View = android.view.View;
+    const window = application.android.startActivity.getWindow();
+    if (color == 'default') {
+      window.setStatusBarColor(defaultColor);
+    } else {
+      //存储默认背景色
+      defaultColor = window.getStatusBarColor();
+      const argb = new Color(color);
+      window.setStatusBarColor(argb._argb);
+    }
 
-  // window.setStatusBarColor(0x008000);
-  // window.setStatusBarColor(0xff0000ff);
-  //以下都将被转化为argb颜色
-  // console.log('自己的颜色：', new Color('#FF0000'));
-  // console.log('自己的颜色：', new Color(0xff0000ff));
-  // console.log('自己的颜色：', new Color('blue'));
-  // console.log('安卓的颜色', android.graphics.Color.parseColor('#000000'));
+    // window.setStatusBarColor(0x008000);
+    // window.setStatusBarColor(0xff0000ff);
+    // 以下都将被转化为argb颜色
+    // console.log('自己的颜色：', new Color('#FF0000'));
+    // console.log('自己的颜色：', new Color(0xff0000ff));
+    // console.log('自己的颜色：', new Color('blue'));
+    // console.log('安卓的颜色', android.graphics.Color.parseColor('#000000'));
 
-  //判断亮色暗色，设置导航栏字体颜色
-  const colorChange = window.getStatusBarColor();
-  console.log('现在的颜色：', colorChange);
-  const v = androidx.core.graphics.ColorUtils.calculateLuminance(colorChange);
-  console.log('亮度处理后的值是：', v);
-  const decorView = window.getDecorView();
-  if (v >= 0.5) {
-    console.log('是亮色,设置导航栏为黑色字体');
-    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-    //设置黑色状态栏字体
-  } else {
-    console.log('是暗色，设置导航栏为白色字体');
-    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+    //判断亮色暗色，设置导航栏字体颜色
+    const colorChange = window.getStatusBarColor();
+    console.log('现在的颜色：', colorChange);
+    const v = androidx.core.graphics.ColorUtils.calculateLuminance(colorChange);
+    console.log('亮度处理后的值是：', v);
+    const decorView = window.getDecorView();
+    if (v >= 0.5) {
+      console.log('是亮色,设置导航栏为黑色字体');
+      decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+      //设置黑色状态栏字体
+    } else {
+      console.log('是暗色，设置导航栏为白色字体');
+      decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+    }
   }
 }
 
