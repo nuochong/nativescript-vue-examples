2f790d4673c5fea6456a36ac5f9c4e7ef349462d
nuochong <nuo.chong@163.com>
2019-09-15 15:57:49 +0800

    update

--- app/components/project/tabbar/tabbar3.vue
+++ app/components/project/tabbar/tabbar3.vue
@@ -1,18 +1,20 @@
 <template>
-  <TabView :selectedIndex="selectedIndex">
-    <TabViewItem title="关注">
-      <Label text="Content for Tab 1" />
-    </TabViewItem>
-    <TabViewItem title="专题">
-      <Label text="Content for Tab 2" />
-    </TabViewItem>
-    <TabViewItem title="连载">
-      <Label text="Content for Tab 3" />
-    </TabViewItem>
-    <TabViewItem title="文集">
-      <Label text="Content for Tab 4" />
-    </TabViewItem>
-  </TabView>
+  <StackLayout>
+    <TabView :selectedIndex="selectedIndex">
+      <TabViewItem title="关注">
+        <Label text="Content for Tab 1" />
+      </TabViewItem>
+      <TabViewItem title="专题">
+        <Label text="Content for Tab 2" />
+      </TabViewItem>
+      <TabViewItem title="连载">
+        <Label text="Content for Tab 3" />
+      </TabViewItem>
+      <TabViewItem title="文集">
+        <Label text="Content for Tab 4" />
+      </TabViewItem>
+    </TabView>
+  </StackLayout>
 </template>
 
 <script>
