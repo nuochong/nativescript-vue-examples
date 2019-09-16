98a0a63f6f6cdc95cf5b620815e30a77b0765a8b
nuochong <nuo.chong@163.com>
2019-09-17 00:11:17 +0800

    update

--- app/components/example/custom-model/modal-component.vue
+++ app/components/example/custom-model/modal-component.vue
@@ -1,25 +1,28 @@
 <template>
-  <StackLayout class="p-20 wrap" backgroundColor="white">
-    <Label class="h2 text-center" text="这是一个模态对话框页面" />
-    <Label class="h2 text-center" text="这是一个模态对话框页面" />
-    <Button class="btn btn-outline" text="关闭模态对话框" @tap="$modal.close()" />
-    <Button class="btn btn-outline" text="关闭模态对话框" @tap="$modal.close()" />
+  <StackLayout class="container">
+    <StackLayout class="wrap">
+      <Label class="h2 text-center model-label" text="这是一个模态对话框页面" />
+      <Button class="btn btn-outline" text="关闭模态对话框" @tap="$modal.close()" />
+    </StackLayout>
   </StackLayout>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      title: ''
-    };
+    return {};
   }
 };
 </script>
 
 <style scoped lang="scss">
 .wrap {
-  padding-top: 100px;
+  padding: 15;
+  background-color: white;
+}
+.model-label {
+  color: black;
+  font-size: 20;
 }
 .btn {
   font-size: 18;
