f86aa91a490f72ac0d3aadf20df9cd22d8be15b9
nuochong <nuo.chong@163.com>
2019-09-17 16:21:49 +0800

    update

--- app/router/router.js
+++ app/router/router.js
@@ -2,78 +2,9 @@ import Example from '~/components/example/main';
 import Project from '~/components/project/main';
 import World from '~/components/example/public/world';
 
-import Counter from '~/components/example/counter';
-import Camera from '~/components/example/camera';
-import Login from '~/components/example/login';
-import Toast from '~/components/example/toast';
-import FancyAlert from '~/components/example/fancy-alert';
-import Info from '~/components/example/info';
-import DialogNew from '~/components/example/dialog-new';
-import PlatformModule from '~/components/example/platform-module';
-import Phone from '~/components/example/phone';
-import Geolocation from '~/components/example/geolocation';
-import Permission from '~/components/example/permission';
-import ChangeBtn from '~/components/example/change-btn';
-import Display from '~/components/example/display';
-
-import Target from '~/components/example/target/target';
-import Storage from '~/components/example/storage';
-import Fab from '~/components/example/fab/fab';
-import Todo from '~/components/example/todo/todo';
-import IconMain from '~/components/example/icon/icon-main';
-import Mask from '~/components/example/mask';
-import Modal from '~/components/example/custom-model/modal';
-import Drawer from '~/components/example/multi-drawer/drawer';
-import LargeTitle from '~/components/example/large-title/large-title';
-import Pop from '~/components/example/pop';
-import Animate from '~/components/example/animate';
-import Refresh from '~/components/example/refresh';
-import RefreshNew from '~/components/example/refresh-new';
-import Touch from '~/components/example/touch';
-import RequestMain from '~/components/example/request/request-main';
-import Qrcode from '~/components/example/qrcode/qrcode';
-import Webview from '~/components/example/webview/webview';
-import Effect from '~/components/example/effect';
-import Popupmenu from '~/components/example/popupmenu';
-import ShareMain from '~/components/example/share/share-main';
-
 const router = {
-  World,
   Example,
   Project,
-  Counter,
-  Camera,
-  Login,
-  Toast,
-  FancyAlert,
-  Info,
-  DialogNew,
-  PlatformModule,
-  Phone,
-  Geolocation,
-  Permission,
-  ChangeBtn,
-  Display,
-
-  Target,
-  Storage,
-  Fab,
-  Todo,
-  IconMain,
-  Mask,
-  Modal,
-  Drawer,
-  LargeTitle,
-  Pop,
-  Animate,
-  Refresh,
-  RefreshNew,
-  Touch,
-  RequestMain,
-  Qrcode,
-  Webview,
-  Effect,
-  Popupmenu,
-  ShareMain
+  World
 };
 export default router;
