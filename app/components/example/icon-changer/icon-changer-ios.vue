1fdd3faf5b9bdc1d75fc11b29239786b98b52c74
nuochong <nuo.chong@163.com>
2019-09-24 17:21:40 +0800

    update

---
+++ app/components/example/icon-changer/icon-changer-ios.vue
@@ -0,0 +1,39 @@
+<template>
+  <Page class="page">
+    <ActionBarSecond :title="title" />
+    <ScrollView>
+      <StackLayout class="hello-world">
+          <Button class="btn btn-primary" text="修改IOS图标" @tap="btnChange" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+var AppIconChangerPlugin = require('nativescript-app-icon-changer');
+var appIconChanger = new AppIconChangerPlugin.AppIconChanger();
+import ActionBarSecond from './public/action-bar-second';
+
+export default {
+  components: {
+    ActionBarSecond
+  },
+  data() {
+    return {
+      title: '修改IOS图标',
+    };
+  },
+  mounted() {},
+  methods: {
+    btnChange() {
+      appIconChanger.changeIcon({
+        iconName: 'icon-blue', // or null to reset to the default
+        suppressUserNotification: true
+      });
+    },
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ app/components/example/icon-changer/icon-changer-main.vue
@@ -0,0 +1,36 @@
+<template>
+  <Page class="page">
+    <ActionBarSecond :title="title" />
+
+    <ScrollView>
+      <StackLayout class="hello-world">
+        <Button class="btn btn-primary" text="修改IOS图标" @tap="btnIconChangerIos" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import IconChangerIos from './icon-changer-ios';
+import ActionBarSecond from '../public/action-bar-second';
+
+export default {
+  components: {
+    ActionBarSecond
+  },
+  data() {
+    return {
+      title: '修改图标'
+    };
+  },
+  mounted() {},
+  methods: {
+    btnIconChangerIos: function() {
+      this.navigateTo(this, IconChangerIos);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
