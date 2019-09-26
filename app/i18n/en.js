c67a1bfaa5ce3070b867bf2ba197945ff8bc6e83
nuochong <nuo.chong@163.com>
2019-09-26 17:59:38 +0800

    update

--- app/i18n/en.js
+++ app/i18n/en.js
@@ -3,17 +3,17 @@ module.exports = {
   // app_name: 'NSVue Examples',
   title_activity_kimera: 'NSVue Examples',
   // 这里是android长按菜单，也同样会被编译到ios，该错误需要处理
-  shortcut_disabled_message1: '功能1-disabled',
-  shortcut_long_label1: '功能1-long',
-  shortcut_short_label1: '功能1-short',
+  shortcut_disabled_message1: 'function1-disabled',
+  shortcut_long_label1: 'function1-long',
+  shortcut_short_label1: 'function1-short',
 
-  shortcut_disabled_message2: '功能2-disabled',
-  shortcut_long_label2: '功能2-long',
-  shortcut_short_label2: '功能2-short',
+  shortcut_disabled_message2: 'function2-disabled',
+  shortcut_long_label2: 'function2-long',
+  shortcut_short_label2: 'function2-short',
 
-  shortcut_disabled_message3: '功能3-disabled',
-  shortcut_long_label3: '功能3-long',
-  shortcut_short_label3: '功能3-short',
+  shortcut_disabled_message3: 'function3-disabled',
+  shortcut_long_label3: 'function3-long',
+  shortcut_short_label3: 'function3-short',
 
   // 社交登录start
   facebook_app_id: '{{ YOUR_FACEBOOK_APP_ID }}',
