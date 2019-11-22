8ab7af36643701fac169b3397e0511156930a598
nuochong <nuo.chong@163.com>
2019-11-22 14:56:48 +0800

    update router

---
+++ app/router/index.js
@@ -0,0 +1,36 @@
+// 原生Vue-router
+import Vue from 'vue';
+import Router from 'vue-router';
+import Example from '~/components/example/main';
+import Project from '~/components/project/main';
+import World from '~/components/example/public/world';
+Vue.use(Router);
+
+let router = new Router({
+  mode: 'abstract',
+  // saveScrollPosition: true,
+  // transitionOnLoad: true,
+  // base: process.env.ITEM_PATH + process.env.ITEM_NAME,
+  routes: [
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example,
+      meta: { title: '示例', keepAlive: true }
+    },
+    {
+      path: '/project',
+      name: 'Project',
+      component: Project,
+      meta: { title: '实例', keepAlive: true }
+    },
+    {
+      path: '/world',
+      name: 'World',
+      component: World,
+      meta: { title: 'World', keepAlive: false }
+    }
+  ]
+});
+
+export default router;
--- app/router/router.js
+++ app/router/router.js
@@ -1,3 +1,4 @@
+// 仿制Vue-router
 import Example from '~/components/example/main';
 import Project from '~/components/project/main';
 import World from '~/components/example/public/world';
